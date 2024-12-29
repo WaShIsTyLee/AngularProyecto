387ffd280ea8baed63df64637f37b6aea50e3561
MariaPas235 <151913868+MariaPas235@users.noreply.github.com>
2024-12-30 00:18:31 +0100

    autenticación google

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
-    provideFirebaseApp(() =>
-      initializeApp(environment.firestore)
-    ),
+    provideFirebaseApp(() => initializeApp(environment.firestore)),
     provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth())
+    
   ],
 };
